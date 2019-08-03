d8c14008f1a78cecf4fcdf69a81e4677c642c79f
Rachael Sewell <rachmari@github.com>
2019-08-03 14:44:52 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+
+async function run() {
+  try {
+    const nameToGreet = core.getInput('who-to-greet');
+        core.debug(`Hello ${nameToGreet}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
