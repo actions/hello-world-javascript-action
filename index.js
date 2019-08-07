c7b08592b7c4b59d29f3105491626c7907f80423
Rachael Sewell <rachmari@github.com>
2019-08-06 20:27:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/core');
 
-
 try {
   const nameToGreet = core.getInput('who-to-greet');
-      core.debug(`Hello ${nameToGreet}!`);
+      console.log(`Hello ${nameToGreet}!`);
       const time = (new Date()).toTimeString();
       core.setOutput("time", time);
+  const context = github.context;
+  core.debug(`The webhook payload: ${context.payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
