bc3c29f84d0db49de901c66ebdc87edda9d8489f
Rachael Sewell <rachmari@github.com>
2019-08-06 19:24:57 -0700

    Remove async function wrapper

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/core');
 
-async function run() {
-  try {
-    const nameToGreet = core.getInput('who-to-greet');
-        core.debug(`Hello ${nameToGreet}!`);
-        const time = (new Date()).toTimeString();
-        core.setOutput("time", time);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
 
-run();
+try {
+  const nameToGreet = core.getInput('who-to-greet');
+      core.debug(`Hello ${nameToGreet}!`);
+      const time = (new Date()).toTimeString();
+      core.setOutput("time", time);
+} catch (error) {
+  core.setFailed(error.message);
+}
