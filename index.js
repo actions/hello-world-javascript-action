6f20eea4033fcbe3da4421fdd9244f602960636f
Aaron Roller <aaron.roller@automodality.com>
2020-01-01 11:33:56 -0800

    trying out pr branch name pr-branch

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  console.log(`Branch: ${context.payload.pull_request.head.ref}`)
+  console.log(`Branch from PR: ${context.payload.pull_request.head.ref}`)
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
